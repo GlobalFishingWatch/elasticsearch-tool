073dcb4d1772c9a760423fd6378ed36134ced0f0
aperdizs <info@alvaroperdiz.com>
2022-01-04 12:33:10 +0100

    Export functions as a package to use the tool as a library. PIPELINE-595

---
+++ pkg/actions.go
@@ -0,0 +1,23 @@
+package pkg
+
+import (
+	"github.com/GlobalFishingWatch/elasticsearch-tool/internal/action"
+	"github.com/GlobalFishingWatch/elasticsearch-tool/types"
+)
+
+func AddAlias(params types.AddAliasParams) {
+	action.AddAlias(params)
+}
+
+func CreateIndexWithCustomMapping(params types.CreateIndexParams) {
+	action.CreateIndexWithCustomMapping(params)
+
+}
+
+func DeleteIndex(params types.DeleteIndexParams) {
+	action.DeleteIndex(params)
+}
+
+func DeleteIndicesByPrefix(params types.DeleteIndicesByPrefixParams) {
+	action.DeleteIndicesByPrefix(params)
+}
